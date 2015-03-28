4a2a76ec8eb6c618464567f2783a9fb45325916e
Chebavictor <order@webcarver.ru>
2015-03-28 22:03:38 +0500

    commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+	$(".mc_toggle").click(function(){
+		$(this).parent().parent().children("ul").slideToggle();
+	});
+
 	//Таймер обратного отсчета
 	//Документация: http://keith-wood.name/countdown.html
 	//<div class="countdown" date-time="2015-01-07"></div>
